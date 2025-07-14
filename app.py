bf98842b371047fe33f4735ff4d2e6e665c67518
Pepe030303 <redog1432@gmail.com>
2025-07-14 20:07:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from collections import defaultdict
 logging.basicConfig(level=logging.INFO)
 
 # 🛠️ 버전 업데이트
-APP_VERSION = "v1.4.1" 
+APP_VERSION = "v1.5.0" 
 
 class Base(DeclarativeBase): pass
 db = SQLAlchemy(model_class=Base)
